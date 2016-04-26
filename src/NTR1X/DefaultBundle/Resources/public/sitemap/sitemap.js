206028347d3ec3dde05921c132575780d4aef7c4
ksetrin <ksetrinn@gmail.com>
2016-04-26 18:13:29 +0500

    add widget: sitemap

---
+++ src/NTR1X/DefaultBundle/Resources/public/sitemap/sitemap.js
@@ -0,0 +1,32 @@
+Core = window.Core || {};
+
+(function($, Vue, Core) {
+
+    //"use strict";
+
+    Core.WidgetMixin = {
+        props: {
+            data: Object,
+            params: Object,
+        },
+        data: function() {
+            return {
+                fetchData: this.fetchData
+            }
+        },
+        ready: function() {
+
+            var self = this;
+            $.get(this.params.source.value, function( data ) {
+                self.fetchData = data;
+            });
+
+        }
+    };
+
+    Vue.component('default-sitemap', {
+        template: '#default-sitemap',
+        mixins: [ Core.WidgetMixin ]
+    });
+
+})(jQuery, Vue, Core);
