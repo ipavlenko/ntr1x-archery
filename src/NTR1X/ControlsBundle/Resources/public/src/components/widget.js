bf92d7f67a19a4d2bb265cb0af967c46e5ffc4d9
ksetrin <ksetrinn@gmail.com>
2016-05-05 11:01:16 +0500

    Move up WidgetMixin

---
+++ src/NTR1X/ControlsBundle/Resources/public/src/components/widget.js
@@ -0,0 +1,12 @@
+Core = window.Core || {};
+
+(function(Vue, $, Core) {
+
+    Core.WidgetMixin = {
+        props: {
+            data: Object,
+            params: Object,
+        }
+    }
+    
+})(Vue, jQuery, Core, undefined);
