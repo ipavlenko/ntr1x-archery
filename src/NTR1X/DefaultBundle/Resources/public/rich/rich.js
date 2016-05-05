957cf486bbfcaf8ea7a3c650c2bf0c2c93bd03ce
ksetrin <ksetrinn@gmail.com>
2016-05-05 11:03:33 +0500

    add widget: rich (Warning: uses type string)

---
+++ src/NTR1X/DefaultBundle/Resources/public/rich/rich.js
@@ -0,0 +1,11 @@
+Core = window.Core || {};
+
+(function($, Vue, Core) {
+    
+    Vue.component('default-rich', {
+        template: '#default-rich',
+        mixins: [ Core.WidgetMixin ],
+        
+    });
+
+})(jQuery, Vue, Core);
