bc29ff311f0e07ca6b845146d2a3e3dc3b7beb28
Igor Pavlenko <ipavlenko@inbox.ru>
2016-05-10 23:51:40 +0500

    Watch on selection

--- src/NTR1X/LayoutBundle/Resources/public/src/shell/shell.js
+++ src/NTR1X/LayoutBundle/Resources/public/src/shell/shell.js
@@ -19,36 +19,103 @@
         },
         ready: function() {
 
-            // TODO Тут игнорируется тот факт, что запись может быть с _action: removed
+            function relevant(current, collection) {
 
-            if (!this.selection.category) {
-                if (this.settings.categories.length > 0) {
-                    var sub = this.settings.categories[0];
-                    if (sub.categories.length > 0) {
-                        this.selection.category = sub.categories[0];
+                if (!current || current._action == 'remove' || (collection && collection.indexOf(current) < 0)) {
+
+                    if (collection) {
+                        for (var i = 0; i < collection.length; i++) {
+                            var c = collection[i];
+                            if (c._action != 'remove') {
+                                return c;
+                            }
+                        }
+                    }
+
+                    return null;
+                }
+
+                if (current && current._action == 'remove') {
+                    return null;
+                }
+
+                return current;
+            }
+
+            this.$watch('settings.categories', (categories) => {
+
+                var category = null;
+                if (categories.length > 0) {
+                    var sub = categories[0];
+                    if (categories.length > 0) {
+                        category = sub.categories[0];
                     }
                 }
-            }
+                this.selection.category = category;
+            }, {
+                immediate: true,
+            });
 
-            if (!this.selection.domain) {
-                if (this.model.domains.length > 0) {
-                    this.selection.domain = this.model.domains[0];
-                    this.selection.page = null;
-                    this.selection.source = null;
-                }
-            }
+            this.$watch('model.domains', (domains) => {
+                this.selection.domain = relevant(this.selection.domain, domains);
+            }, {
+                immediate: true,
+            });
 
-            if (!this.selection.page) {
-                if (this.model.pages.length > 0) {
-                    this.selectPage(this.model.pages[0]);
-                }
-            }
+            this.$watch('model.pages', (pages) => {
+                this.selection.page = relevant(this.selection.page, pages);
+            }, {
+                immediate: true,
+            });
 
-            if (!this.selection.source) {
-                if (this.selection.page && this.selection.page.sources.length > 0) {
-                    this.selection.source = this.selection.page.sources[0];
+            this.$watch('selection.page.sources', (sources) => {
+                this.selection.source = relevant(this.selection.source, sources);
+                // console.log(sources, this.selection.source);
+            }, {
+                immediate: true,
+            });
+
+            this.$watch('selection.page.sources', (sources) => {
+
+                this.data = {};
+
+                if (sources) {
+                    for (var i = 0; i < sources.length; i++) {
+
+                        var s = sources[i];
+
+                        var query = {};
+                        for (var i = 0; i < s.params.length; i++) {
+                            var param = s.params[i];
+                            if (param.in == 'query' && param.specified) {
+
+                                var value = param.binding
+                                    ? this.$interpolate(param.binding) // TODO Interpolate in page context
+                                    : param.value
+                                ;
+
+                                query[param.name] = value;
+                            }
+                        }
+
+                        $.ajax({
+                            method: s.method,
+                            url: s.url,
+                            dataType: "json",
+                            data: query,
+
+                        })
+                        .done((d) => {
+                            this.data[s.name] = d;
+                        });
+                    }
                 }
-            }
+
+            }, {
+                immediate: true,
+                deep: true,
+            });
+
         },
         methods: {
 
@@ -86,7 +153,11 @@
                 }
 
                 this.globals.data = data;
-            }
+            },
+
+            selectSource: function(source) {
+
+            },
         },
         events: {
             pull: function(data) {
@@ -121,7 +192,7 @@
                 this.selection.domain = data.item;
             },
             selectPage: function(data) {
-                this.selectPage(data.item);
+                this.selection.page = data.item;
             },
             selectSource: function(data) {
                 this.selection.source = data.item;
