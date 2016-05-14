77598d6fb73944f6ed90db759204749061702785
Igor Pavlenko <ipavlenko@inbox.ru>
2016-05-15 00:26:01 +0500

    Inputs

---
+++ src/NTR1X/DefaultBundle/Resources/public/src/input/input.js
@@ -0,0 +1,18 @@
+(function($, Vue, Core) {
+
+    Vue.component('default-input-text', {
+        template: '#default-input-text',
+        mixins: [ Core.WidgetMixin ],
+    });
+
+    Vue.component('default-input-checkbox', {
+        template: '#default-input-checkbox',
+        mixins: [ Core.WidgetMixin ],
+    });
+
+    Vue.component('default-input-radio', {
+        template: '#default-input-radio',
+        mixins: [ Core.WidgetMixin ],
+    });
+
+})(jQuery, Vue, Core);
