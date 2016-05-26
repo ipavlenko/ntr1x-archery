a1cac60f8653e8060d756c4edc7d6b50319ffe1e
ksetrin <epv01@mail.ru>
2016-05-26 14:39:11 +0500

    fix default widgets

--- src/NTR1X/DefaultBundle/Resources/public/src/table/table.js
+++ src/NTR1X/DefaultBundle/Resources/public/src/table/table.js
@@ -4,22 +4,7 @@ Core = window.Core || {};
 
     Vue.component('default-table', {
         template: '#default-table',
-        mixins: [ Core.WidgetMixin ],
-        data: function() {
-            return {
-                fetchData: this.fetchData
-            }
-        },
-        ready: function() {
-            var self = this;
-            $.ajax({
-                url: "/bundles/ntr1xdefault/src/table/data.json",
-                async: false,
-                dataType: "json"
-            }).success(function( data_r, textStatus, jqXHR ) {
-                self.fetchData = data_r;
-            });
-        }
+        mixins: [ Core.WidgetMixin ]
     });
 
 })(jQuery, Vue, Core);
