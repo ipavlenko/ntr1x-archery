f4388e0d75c7fe03699addc926c071a564f16e18
ksetrin <epv01@mail.ru>
2016-05-26 14:38:11 +0500

    add MGA Bundle

---
+++ src/NTR1X/MgaBundle/Resources/public/dist/scripts.js
@@ -0,0 +1,14 @@
+Core = window.Core || {};
+
+(function($, Vue, Core) {
+
+    Vue.component('mga-gallery', {
+        template: '#mga-gallery',
+        mixins: [ Core.WidgetMixin ]
+    });
+
+
+
+})(jQuery, Vue, Core);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImdhbGxlcnkvZ2FsbGVyeS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJzY3JpcHRzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiQ29yZSA9IHdpbmRvdy5Db3JlIHx8IHt9O1xyXG5cclxuKGZ1bmN0aW9uKCQsIFZ1ZSwgQ29yZSkge1xyXG5cclxuICAgIFZ1ZS5jb21wb25lbnQoJ21nYS1nYWxsZXJ5Jywge1xyXG4gICAgICAgIHRlbXBsYXRlOiAnI21nYS1nYWxsZXJ5JyxcclxuICAgICAgICBtaXhpbnM6IFsgQ29yZS5XaWRnZXRNaXhpbiBdXHJcbiAgICB9KTtcclxuXHJcblxyXG5cclxufSkoalF1ZXJ5LCBWdWUsIENvcmUpO1xyXG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
---
+++ src/NTR1X/MgaBundle/Resources/public/src/gallery/gallery.js
@@ -0,0 +1,12 @@
+Core = window.Core || {};
+
+(function($, Vue, Core) {
+
+    Vue.component('mga-gallery', {
+        template: '#mga-gallery',
+        mixins: [ Core.WidgetMixin ]
+    });
+
+
+
+})(jQuery, Vue, Core);
