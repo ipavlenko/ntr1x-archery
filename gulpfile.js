ad981c283edff321b0f803e0580a32fa235da83e
ipavlenko <ipavlenko@inbox.ru>
2016-08-03 13:10:52 +0500

    Structure update

--- gulpfile.js
+++ gulpfile.js
@@ -86,6 +86,17 @@ gulp.task('dev-templates', function() {
         .pipe(gulp.dest('web/assets/dist/htm/'));
 })
 
+gulp.task('dev-images', function() {
+    return streamqueue(
+        { objectMode: true },
+        gulp.src([ '../ntr1x-archery-core/src/**/*.{jpg,png}' ]).pipe(gulp.dest('web/assets/vendor/ntr1x-archery-core/src/')),
+        gulp.src([ '../ntr1x-archery-shell/src/**/*.{jpg,png}' ]).pipe(gulp.dest('web/assets/vendor/ntr1x-archery-shell/src/')),
+        gulp.src([ '../ntr1x-archery-widgets/src/**/*.{jpg,png}' ]).pipe(gulp.dest('web/assets/vendor/ntr1x-archery-widgets/src/')),
+        gulp.src([ '../ntr1x-archery-landing/src/**/*.{jpg,png}' ]).pipe(gulp.dest('web/assets/vendor/ntr1x-archery-landing/src/')),
+        gulp.src([ '../ntr1x-archery-widgets-academy/src/**/*.{jpg,png}' ]).pipe(gulp.dest('web/assets/vendor/ntr1x-archery-widgets-academy/src/'))
+    );
+})
+
 gulp.task('build', function() {
     return gulp.src(['app/Resources/views-src/public.html.twig'])
         .pipe(fileinclude({
@@ -102,6 +113,7 @@ gulp.task('default', [
     'dev-scripts',
     'dev-styles',
     'dev-templates',
+    'dev-images',
 ], function() {
     gulp.start('build')
 })
@@ -129,6 +141,12 @@ gulp.task('watch', function() {
     gulp.watch('../ntr1x-archery-landing/src/**/*.htm', [ 'dev-templates' ])
     gulp.watch('../ntr1x-archery-widgets/src/**/*.htm', [ 'dev-templates' ])
     gulp.watch('../ntr1x-archery-widgets-academy/src/**/*.htm', [ 'dev-templates' ])
+
+    gulp.watch('../ntr1x-archery-core/src/**/*.{jpg,png}', [ 'dev-images' ])
+    gulp.watch('../ntr1x-archery-shell/src/**/*.{jpg,png}', [ 'dev-images' ])
+    gulp.watch('../ntr1x-archery-landing/src/**/*.{jpg,png}', [ 'dev-images' ])
+    gulp.watch('../ntr1x-archery-widgets/src/**/*.{jpg,png}', [ 'dev-images' ])
+    gulp.watch('../ntr1x-archery-widgets-academy/src/**/*.{jpg,png}', [ 'dev-images' ])
 
     gulp.watch('app/Resources/views-src/public.html.twig', [ 'build' ])
     gulp.watch('web/assets/dist/htm/app.htm', [ 'build' ])
